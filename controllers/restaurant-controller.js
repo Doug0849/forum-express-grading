c156e2160cc924565e28a9148c9aa9dff3d64818
doug0849 <etaomonk@gmail.com>
2022-07-18 23:55:20 +0800

    revise restaurant top 10

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,6 @@
 const { Restaurant, Category, Comment, User, Followship, Favorite } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const sequelize = require('sequelize')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
@@ -87,7 +88,6 @@ const restaurantController = {
   getTopRestaurants: async (req, res, next) => {
     try {
       let restaurants = await Restaurant.findAll({
-        limit: 10,
         include: [{ model: User, as: 'FavoritedUsers' }]
       })
       const FavoritedRestaurants = req.user ? req.user.FavoritedRestaurants : []
@@ -98,6 +98,7 @@ const restaurantController = {
         isFavorited: FavoritedRestaurants.some(fr => fr.id === restaurant.id)
       }))
       await restaurants.sort((a, b) => b.favoritedCount - a.favoritedCount)
+      restaurants.splice(10)
       return res.render('top-restaurants', { restaurants })
     } catch (error) {
       next(error)
